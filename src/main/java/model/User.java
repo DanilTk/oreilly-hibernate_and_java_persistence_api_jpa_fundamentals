66d61c275b49eef98f468358623fec3821d0bf2a
Danylo Tkachenko <d.y.tkachenko@gmail.com>
2020-10-14 22:38:03 +0200

    Initial commit

---
+++ src/main/java/model/User.java
@@ -0,0 +1,60 @@
+package model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "finances_user")
+public class User {
+    @Id
+    @Column(name = "USER_ID")
+    private Long userId;
+
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+
+    @Column(name = "LAST_NAME")
+    private String lastName;
+
+    @Column(name = "BIRTH_DATE")
+    private LocalDate birthDate;
+
+    @Column(name = "EMAIL_ADDRESS")
+    private String emailAddress;
+
+    @Column(name = "LAST_UPDATED_BY")
+    private String lastUpdatedBy;
+
+    @Column(name = "LAST_UPDATED_DATE")
+    private LocalDateTime lastUpdatedDate;
+
+    @Column(name = "CREATED_BY")
+    private String createdBy;
+
+    @Column(name = "CREATED_DATE")
+    private LocalDateTime createdDate;
+
+    @Column(name = "USER_ADDRESS_LINE_1")
+    private String userAddressLine1;
+
+    @Column(name = "USER_ADDRESS_LINE_2")
+    private String userAddressLine2;
+
+    @Column(name = "CITY")
+    private String city;
+
+    @Column(name = "STATE")
+    private String state;
+
+    @Column(name = "ZIP_CODE")
+    private String zipCode;
+}
